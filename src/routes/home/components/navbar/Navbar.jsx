0b90bf4a85539033acd4a4762d254b92e04c4e9f
Rai <raibr16@gmail.com>
2022-04-20 10:10:01 +0200

    Test home page

---
+++ src/routes/home/components/navbar/Navbar.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import './NavbarStyles.css'
+import { Link } from 'react-scroll/'
+import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+import { AiOutlineClose } from 'react-icons/ai'
+import { Link as Element } from "react-router-dom";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
+
+
+
+
+
+function Navbar() {
+
+    const [nav, setNav] = useState(false)
+    const handleNav = () => setNav(!nav)
+
+    const [color, setColor] = useState(false)
+    const changeColor = () => {
+        if (window.scrollY >= 100) {
+            setColor(true)
+        } else {
+            setColor(false)
+        }
+    }
+
+    window.addEventListener(`scroll`, changeColor)
+
+    return (
+
+        <div name="home" className={nav ? "navbar navbar-bg" : "navbar"}>
+            <div className={nav ? "logo dark" : "logo"}>
+                <h2 className="logo">MOVIE BAZAR</h2>
+            </div>
+            <ul className="nav-menu">
+                <Link to="home" className="link-homee" smooth="true" duration={500}><li>Home</li></Link>
+                <Link to="gallery" className="link-gallery" smooth="true" duration={500}><li>Filmes</li></Link>
+            </ul>
+            <div className="nav-icons">
+                <AiOutlineShoppingCart className="icon" />
+            </div>
+
+            <div className="hamburger" onClick={handleNav}>
+                {!nav ? (<HiOutlineMenuAlt4 className="icon" />) : (<AiOutlineClose style={{ color: "#000" }} className="icon" />)}
+            </div>
+            <div className={nav ? "mobile-menu active" : "mobile-menu"}>
+                <ul className="mobile-nav">
+                    <Link to="home" smooth="true" duration={500}><li>Home</li></Link>
+                    <Link to="filmes" smooth="true" duration={500}><li>Filmes</li></Link>
+
+                </ul>
+                <div className="mobile-menu-botton">
+                    <div className="menu-icons">
+                        <Link  >
+                            <button>Filmes</button>
+                        </Link>
+                    </div>
+                    <div className="social-icons">
+                        <FaFacebook className="icon" />
+                        <FaInstagram className="icon" />
+                        <FaYoutube className="icon" />
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Navbar

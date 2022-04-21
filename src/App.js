21bd7d6f6befb9219ddf0c267bd715cb8debfc8e
Rai <raibr16@gmail.com>
2022-04-21 13:30:40 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import './App.css';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './routes/home/Home';
 
 
 function App() {
 
-
-
-
-
-
-    return ( <
-        main >
-        <
-        Routes >
-        <
-        Route exact path = "/"
-        element = { < Home / > }
-        /> < /
-        Routes > <
-        /main>
+  return (
+    <main>
+      <Routes >
+        <Route exact path="/"element={< Home />}/>
+      </Routes >
+      </main>
     );
 }
 export default App;

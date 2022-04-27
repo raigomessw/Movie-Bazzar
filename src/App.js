a30e11a1249da2cff13d6ce85ae4e67c369a28af
Rai <raibr16@gmail.com>
2022-04-27 03:19:39 +0200

    fix grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './routes/home/Home';
 
 
 function App() {
-
-  // The use of API from TMDB //
-
-// Info about the API : https://www.themoviedb.org/documentation/api
-// Examples with the API key: https://www.themoviedb.org/documentation/api/discover
-
-
-
-/*const url = 'https://api.themoviedb.org/3'
-const apiKey = 'api_key=25dcea31f980ad3c4032d87bbaa6778a'
-
-// Show top popular movies. Useful form home
-const popularMoviesHome = '/discover/movie?sort_by=popularity.desc&'
-
-// Search throught API
-const searchMovie = '/search/movie?'*/
 
   return (
     <main>

fbccabe6103cad090cc382c95fa1a070f14cfd9c
Rai <raibr16@gmail.com>
2022-05-20 10:11:43 +0200

    Updates

---
+++ precache-manifest.cec104b595ea82d456bd623aafd0143d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "861507b338f299192404",
+    "url": "/Movie-Bazzar/static/css/main.05c66a11.chunk.css"
+  },
+  {
+    "revision": "861507b338f299192404",
+    "url": "/Movie-Bazzar/static/js/main.3e3f46f7.chunk.js"
+  },
+  {
+    "revision": "6a95d43d63ac17a12243",
+    "url": "/Movie-Bazzar/static/js/runtime~main.37b062ad.js"
+  },
+  {
+    "revision": "cad57fa36595495eec50",
+    "url": "/Movie-Bazzar/static/css/2.626b7e48.chunk.css"
+  },
+  {
+    "revision": "cad57fa36595495eec50",
+    "url": "/Movie-Bazzar/static/js/2.39d51190.chunk.js"
+  },
+  {
+    "revision": "c71eb7c33ea98db2f24e36de55a07334",
+    "url": "/Movie-Bazzar/static/media/logotipo.c71eb7c3.gif"
+  },
+  {
+    "revision": "07e915ca0f817717781b70b6183549e4",
+    "url": "/Movie-Bazzar/index.html"
+  }
+];
